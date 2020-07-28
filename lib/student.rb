c3a3ca895630257eec30d3fd173c1d6d35af6a93
Jorge Ramirez <joramirez96@gmail.com>
2020-07-28 23:50:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,10 +38,9 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
-  def self.create()
-    sql = <<-SQL
+  def self.create(name:, grade:)
+    
 
-    SQL
   end
 
 
